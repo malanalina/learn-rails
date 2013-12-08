a0d70f43fa8ae5a8565ca93966cc675503df129a
malanalina <alana.malina@gmail.com>
2013-12-08 16:50:54 -0500

    updated Application layout by using the RailsLayout gem

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,8 @@ class VisitorsController < ApplicationController
 	def new 
 		Rails.logger.debug 'DEBUG: entering new Method'
 		@owner = Owner.new
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alert] = 'My birthday is soon.'
 		Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
 	end
 	
