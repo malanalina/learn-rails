dafc9091cfb38c6fda6213d10e5101e3f58ff5e7
malanalina <alana.malina@gmail.com>
2013-12-08 16:12:01 -0500

    Exception handling exercise (no change should be committed here), example ruby code

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,9 @@
 class VisitorsController < ApplicationController
 
 	def new 
+		Rails.logger.debug 'DEBUG: entering new Method'
 		@owner = Owner.new
+		Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
 	end
 	
 end
